be20fdaaaa37fbf73d490f68ad0ca07a5070ad6e
Akash Davesar <a.davesar@hotmail.com>
2019-04-05 00:25:19 -0700

    setup for react-native-sqlite-storage

--- android/app/src/main/java/com/getliquored/MainApplication.java
+++ android/app/src/main/java/com/getliquored/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,9 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+          new RNCameraPackage(),
+          new VectorIconsPackage(),
+          new RNGestureHandlerPackage(),
+          new SQLitePluginPackage(), 
+          new MainReactPackage()
       );
     }
 
